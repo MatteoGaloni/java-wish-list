cf0617e81437e958b77853a44c1a08798d6be470
MatteoGaloni <mtgaloni@gmail.com>
2023-10-26 15:03:59 +0200

    Add Gift Class - print items in console

--- src/org/lessons/java/christmas/Gift.java
+++ src/org/lessons/java/christmas/Gift.java
@@ -1,2 +1,36 @@
-package org.lessons.java.christmas;public class Gift {
+package org.lessons.java.christmas;
+
+public class Gift {
+
+    private String recipient;
+    private String description;
+
+    public Gift(String recipient, String description) {
+        this.recipient = recipient;
+        this.description = description;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "GiftList{" +
+                "recipient='" + recipient + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,2 +1,37 @@
-package org.lessons.java.christmas;public class Main {
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<Gift> giftList = new ArrayList<>();
+
+        boolean stop = false;
+
+        while(!stop){
+            System.out.println("Would you add a gift to you list? Press y or n");
+            stop = scan.nextLine().equals("n");
+
+            if (!stop) {
+                System.out.println("Who's the gift for?");
+                String recipient = scan.nextLine();
+
+                System.out.println("Please enter the gift description");
+                String description = scan.nextLine();
+
+                giftList.add(new Gift(recipient,description));
+                System.out.println("Your list contains " + giftList.size() + " items");
+            }
+
+        }
+
+        System.out.println(giftList);
+
+        scan.close();
+    }
 }
